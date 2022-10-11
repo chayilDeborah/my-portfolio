a2621ca3c9f3cbedf67fe11423561c4ec03624e2
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-11 13:24:57 +0100

    complete projects section

--- src/data.js
+++ src/data.js
@@ -1,5 +1,23 @@
 export const projects = [
     {
-        
+        title: "CRWN-Clothing(E-Commerce Website)",
+        subtitle: "React, Redux, Firebase, Styled component, Stripe",
+        description: "An E-Commerce website built with ReactJS, Redux for state management, used Firebase for authentication and to manage users; CSS in JS for styling and implemented Stripe payment gateway",
+        image: "./crwn.gif",
+        link: "https://crwn-clothing-tan.vercel.app/",
+    },
+    {
+        title: "Monsters Rolodex",
+        subtitle: "React, Pure CSS, API",
+        description: "A Single Page Application built with ReactJS, consumed API to fetch list of monsters images",
+        image: "./monster.gif",
+        link: "https://monsters-app-2.vercel.app/",
+    },
+    {
+        title: "Gentlemen's Crib",
+        subtitle: "React, Styled component, ContextAPI, Paystack",
+        description: "A men's fashion webiste built with React, used ContextAPI for state management and implemented Paystack Paystack payment gateway ",
+        image: "./gmc.gif",
+        link: "https://starterpacks.vercel.app/"
     }
-]
+];
