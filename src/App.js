c3548e38b642d26821e914522df25db46e3ce366
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-11 14:59:03 +0100

    create Skills section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
+import Skills from "./components/Skills"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Navbar />
     <About />
     <Projects />
+    <Skills />
     </main>
     </>
   )
