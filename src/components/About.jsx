8fc4c81e21448c4f2cf4ee0c78acbba007493897
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-11 11:36:29 +0100

    modify hero image in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img className="object-cover object-center rounded" alt="hero" src="./coding.svg"/>
+                    <img className="object-cover object-center rounded" alt="hero" src="./resized.png"/>
                 </div>
             </div>
         </section>
