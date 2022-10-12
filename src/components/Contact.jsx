a42825f1b13a486ccf676fb61bfce0413f1acfc0
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-12 12:20:25 +0100

    create form input field

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,19 +3,36 @@ const Contact = () => {
     <>
       <section id="contact" className="relative">
         <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/5 md:w-1/2 bg-gray-800 rounded-lg overflow-hidden sm:mr-10 p-4 flex items-end ml-[010%] justify-center">
-           
-            <div className="bg-gray-800 flex flex-wrap py-8 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <div class="mapouter">
+              <div class="gmap_canvas">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  title="map"
+                  id="gmap_canvas"
+                  src="https://maps.google.com/maps?q=lagos,%20Nigeria&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                  frameborder="0"
+                  scrolling="no"
+                  className="absolute inset-0"
+                  marginheight="0"
+                  marginwidth="0"
+                  style={{ filter: "opacity(0.7)" }}
+                ></iframe>
+              </div>
+            </div>
+            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+              <div className=" px-6">
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   ADDRESS
                 </h2>
-                <p className="mt-1">                  Lagos
+                <p className="mt-1">
+                  Lagos
                   <br />
                   Nigeria
                 </p>
               </div>
-              <div className="lg:w-1/2 px-3 mt-4 lg:mt-0">
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   EMAIL
                 </h2>
@@ -29,6 +46,36 @@ const Contact = () => {
               </div>
             </div>
           </div>
+          <form className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              Hire Me
+            </h2>
+            <p className="leading-relaxed mb-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                Name
+              </label>
+              <input type='text'id="name" name="name" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                Email
+              </label>
+              <input type='email' id="email" name="email" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div>
+              <label htmlFor="message" className="leading-7 text-sm text-gray-400" >
+                Message
+              </label>
+              <textarea id="message" name="message" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Submit
+            </button>
+          </form>
         </div>
       </section>
     </>
