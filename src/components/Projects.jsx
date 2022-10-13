d4356830946576217b740eb0ad8f1805c72fa9f3
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-13 13:28:41 +0100

    edit project section text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,9 +9,7 @@ const Projects = () => {
                 <div className="flex flex-col w-full mb-20">
                     <CodeIcon className="mx-auto inline-block w-10 mb-4"/>
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">Web Apps I've Built</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.</p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">With close to a year of learning, researching and reading documentations, I've been able to build these projects with the technologies listed below.</p>
 
                 </div>
                 <div className="flex flex-wrap -m-4">
