3a32d41700dfea269426b7ac93173122b4ce6f62
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-13 13:22:15 +0100

    edit contact email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,7 +64,7 @@ const Contact = () => {
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   EMAIL
                 </h2>
-                <a href="#" className="text-indigo-400 leading-relaxed">
+                <a href="mailto:aniyikayeolamide3@gmail.com" className="text-indigo-400 leading-relaxed">
                   aniyikayeolamide3@gmail.com
                 </a>
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
